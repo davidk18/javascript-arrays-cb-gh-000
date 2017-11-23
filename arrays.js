98c85adfcc9a8979e0ff9006bd2b246e3b2b133c
David Kearney <15169235@studentmail.ul.ie>
2017-11-23 11:19:26 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,9 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, element){
-  array.unshift(element);
+  var test = array.unshift(element);
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  
 }
